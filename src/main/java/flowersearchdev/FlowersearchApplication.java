b02c45d8898081adede663a07181f325d328c6f6
softit-ds <softit00001@gmail.com>
2024-06-28 17:29:30 +0900

    Initial commit

---
+++ src/main/java/flowersearchdev/FlowersearchApplication.java
@@ -0,0 +1,13 @@
+package flowersearchdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowersearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowersearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flowersearchdev/FlowersearchApplicationTests.java
@@ -0,0 +1,13 @@
+package flowersearchdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowersearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
